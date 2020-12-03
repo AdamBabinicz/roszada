75defaa58cd120c9b299399d88bb8f81fcb8813c
hipek <puaro@vp.pl>
2020-12-03 17:41:42 +0100

    zmiany

---
+++ js/main.js
@@ -0,0 +1,51 @@
+// Select element function
+const selectElement = function (element) {
+  return document.querySelector(element);
+};
+let menuToggler = selectElement(".menu-toggle");
+let body = selectElement("body");
+let navList = selectElement(".nav-list");
+let closeBox = selectElement(".clo");
+let x = selectElement(".popup_box");
+let y = selectElement(".bg");
+
+menuToggler.addEventListener("click", function () {
+  body.classList.toggle("open");
+});
+
+navList.addEventListener("click", function () {
+  body.classList.toggle("open");
+});
+
+closeBox.addEventListener("click", () => {
+  x.classList.add("close");
+  y.classList.remove("bg");
+});
+// Scroll reveal
+window.sr = ScrollReveal();
+
+sr.reveal(".animate-left", {
+  origin: "left",
+  duration: 1000,
+  distance: "25rem",
+  delay: 300,
+});
+
+sr.reveal(".animate-right", {
+  origin: "right",
+  duration: 1000,
+  distance: "25rem",
+  delay: 600,
+});
+sr.reveal(".animate-top", {
+  origin: "top",
+  duration: 1000,
+  distance: "25rem",
+  delay: 600,
+});
+sr.reveal(".animate-bottom", {
+  origin: "bottom",
+  duration: 1000,
+  distance: "25rem",
+  delay: 600,
+});
---
+++ js/script.js
@@ -0,0 +1,18 @@
+var mySwiper = new Swiper(".swiper-container", {
+  // Optional parameters
+
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
